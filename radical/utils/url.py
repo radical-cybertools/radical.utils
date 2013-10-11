8dacd11a9977b0bfebe6ba3fec95a00b92265436
Andre Merzky <andre@merzky.net>
2013-10-10 22:06:08 -0400

    add and apply the copyright fixer

--- radical/utils/url.py
+++ radical/utils/url.py
@@ -1,4 +1,8 @@
 
+__author__    = "Andre Merzky"
+__copyright__ = "Copyright 2013, The SAGA Project"
+__license__   = "MIT"
+
 
 import contrib.urlparse25 as urlparse
 import signatures         as rus
