b213180b14371795733e009d6b6388c7f871a2a6
Ole Weidner <ole.weidner@rutgers.edu>
2013-10-24 20:41:47 +0200

    fixed tests

--- tests/unittests/misc/test_which.py
+++ tests/unittests/misc/test_which.py
@@ -4,7 +4,7 @@ __copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
 
 
-from saga.utils.which import *
+from radical.utils.which import *
 
 def test_which():
     """ Test if 'which' can find things
