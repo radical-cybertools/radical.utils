381ebe7f3e00f631d4a5954aa7aff79a4b25c842
Andre Merzky <andre@merzky.net>
2013-11-11 00:44:49 +0100

    add simple regex matcher

---
+++ tests/unittests/misc/test_regex.py
@@ -0,0 +1,32 @@
+
+import radical.utils as ru
+
+def test_re_string () :
+    """
+    Test if regex matching works
+    """
+
+    txt   = ru.ReString ("The quick brown fox jumps over the lazy dog")
+    tgt_l = [' qu', 'ick brown fox jumps']
+    tgt_d = {'x'  : 'ick brown fox jumps'}
+    
+    with txt // '(\s.u)(?P<x>.*?j\S+)' as res :
+        assert (res)
+        assert (len(res) == len(tgt_l))
+        assert (res      == tgt_l), "%s != %s" % (str(res), str(tgt_l))
+        assert (res[0]   == tgt_l[0])
+        assert (res[1]   == tgt_l[1])
+        assert (res['x'] == tgt_d['x'])
+        assert (res.x    == tgt_d['x'])
+        for i, r in enumerate (res) :
+            assert (r    == tgt_l[i])
+    
+    if  txt // '(rabbit)' :
+        assert (False)
+    
+    elif  txt // '((?:\s).{12,15}?(\S+))' :     # support for full Python regex slang
+        assert (True)
+    
+    else :
+        assert (False)
+
