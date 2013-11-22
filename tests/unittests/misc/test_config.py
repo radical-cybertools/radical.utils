437ce0b7e54ca70a820b1f90c7d120829a376f48
Andre Merzky <andre@merzky.net>
2013-11-22 22:44:42 +0100

    implement conf location set from env -- #2
    see tests/unittests/misc/test_config.py

---
+++ tests/unittests/misc/test_config.py
@@ -0,0 +1,35 @@
+
+
+import os
+import radical.utils.config as ruc
+
+
+# ------------------------------------------------------------------------------
+def test_config () :
+    """ Test if configuration location is picked up from env """
+
+    tmp = '/tmp/radical.util.cfg.%s' % os.getpid ()
+    os.environ ['RADICAL_UTILS_CONFIG'] = tmp
+
+    with  open (tmp, 'w') as tmp_file :
+        tmp_file.write ("[test]\n")
+        tmp_file.write ("key=val\n")
+
+
+    # set the configuration options for this object
+    configurable = ruc.Configurable        ('radical.utils')
+    config       = configurable.get_config ('test')
+
+    assert ('val' == config['key'].get_value ())
+
+    os.remove (tmp)
+
+# ------------------------------------------------------------------------------
+#
+# run tests if called directly
+#
+if __name__ == "__main__":
+    test_config ()
+
+# ------------------------------------------------------------------------------
+
