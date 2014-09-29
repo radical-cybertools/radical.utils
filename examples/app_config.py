001a26b2ce67027b8d89e036dd7607fd8e395dfe
Andre Merzky <andre@merzky.net>
2014-09-30 01:26:53 +0200

    example update

--- examples/app_config.py
+++ examples/app_config.py
@@ -70,15 +70,67 @@ app_config = {
 # }
 USER_CONFIG_PATH = os.environ.get ('HOME', '/tmp') + '/.my_app.cfg' 
 
-# ------------------------------------------------------------------------------
 # load the user config, and merge it with the default config
 user_config = ru.read_json_str (USER_CONFIG_PATH)
 
-# ------------------------------------------------------------------------------
+
 # merge the user config into the app config, so that the user config keys are
 # applied where appropriate
 ru.dict_merge (app_config, user_config, policy='overwrite', wildcards=True)
 
+
 # lets see what we got
 pprint.pprint (app_config)
+
+
+# this should result in :
+#
+# {
+#     'log_level'   : 0,
+#     'scheduler'   : 'rp.SCHED_BACKFILLING',
+#     'resources'   : ['india.furturegrid.org', 'sierra.futuregrid.org'],
+#     'resource_cfg': 
+#     {
+#         '*.futuregrid.org': 
+#         {
+#             'username'     : 'merzky'
+#         },
+#         'india.futuregrid.org': 
+#         {
+#             'account'      : None,
+#             'needs_account': False,
+#             'supports_mpi' : True,
+#             'username'     : 'merzky'
+#         },
+#         'localhost': 
+#         {
+#             'account'      : None,
+#             'needs_account': False,
+#             'supports_mpi' : False,
+#             'username'     : None
+#         },
+#         'sierra.futuregrid.org': 
+#         {
+#             'account'      : None,
+#             'needs_account': False,
+#             'supports_mpi' : True,
+#             'username'     : 'merzky'
+#         },
+#         'stampede.tacc.utexas.edu': 
+#         {
+#             'account'      : None,
+#             'needs_account': True,
+#             'supports_mpi' : True,
+#             'username'     : None
+#         },
+#         'tutorial.radical.org': 
+#         {
+#             'account'      : None,
+#             'needs_account': False,
+#             'supports_mpi' : True,
+#             'username'     : 'tut_007'
+#         }
+#     }
+# }
+
 
