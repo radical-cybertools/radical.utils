9f0a8621010ebb6869701fec88876d0f04666dcc
mturilli <matteo.turilli@gmail.com>
2014-10-26 22:00:36 -0400

    Add sliding window function for list item comparison

--- radical/utils/misc.py
+++ radical/utils/misc.py
@@ -235,5 +235,28 @@ def all_pairs (iterable, n) :
     from itertools import izip
     return izip(*[iter(iterable)]*n)
 
+
 # ------------------------------------------------------------------------------
+# From https://docs.python.org/release/2.3.5/lib/itertools-example.html
+#
+def window (seq, n=2) :
+    """
+    Returns a sliding window (of width n) over data from the iterable"
+    s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ... 
+    """
+
+    from itertools import islice
+
+    it = iter(seq)
+    result = tuple(islice(it, n))
+
+    if len(result) == n :
+        yield result
+
+    for elem in it :
+        result = result[1:] + (elem,)
+        yield result
+
+# ------------------------------------------------------------------------------
+
 
