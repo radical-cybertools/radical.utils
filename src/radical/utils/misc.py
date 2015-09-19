95d7a30803e9b6a288c0a22bd1c015364e97a0a0
Andre Merzky <andre@merzky.net>
2015-09-19 23:06:32 +0200

    this fixes the debug helper
    now sure when it broke, and dont want to gte side-tracked digging, but this looks like a commit is missing.  Maybe I messed up some stash...

--- src/radical/utils/misc.py
+++ src/radical/utils/misc.py
@@ -249,9 +249,9 @@ class DebugHelper (object) :
 
 # ------------------------------------------------------------------------------
 #
-def print_stacktraces (self, a=None, b=None) :
+def print_stacktraces (signum=None, sigframe=None) :
     """
-    a, b exist only to satisfy signal handler signature requirements
+    signum, sigframe exist to satisfy signal handler signature requirements
     """
 
     this_tid = threading.currentThread().ident
@@ -265,9 +265,9 @@ def print_stacktraces (self, a=None, b=None) :
     out  = "===============================================================\n"
     out += "RADICAL Utils -- Debug Helper -- Stacktraces\n"
     try :
-        info = self.get_stacktraces ()
+        info = get_stacktraces ()
     except Exception as e:
-        out += 'skipping frame (%s) [%s]' % (self, type(self))
+        out += 'skipping frame'
         info = None
 
     if info:
@@ -306,7 +306,7 @@ def print_stacktraces (self, a=None, b=None) :
 
 # --------------------------------------------------------------------------
 #
-def get_stacktraces (self) :
+def get_stacktraces () :
 
     id2name = {}
     for th in threading.enumerate():
