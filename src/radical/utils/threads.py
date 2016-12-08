180b4b55788c5b378a9e856723377e394c3dbdd5
Andre Merzky <andre@merzky.net>
2016-12-08 21:30:54 +0300

    clean out and comment on utils thread wrapper

--- src/radical/utils/threads.py
+++ src/radical/utils/threads.py
@@ -15,24 +15,12 @@ import traceback
 import misc  as rumisc
 
 
-_out_lock = threading.RLock()
-
-
 # ------------------------------------------------------------------------------
 #
 NEW     = 'New'
 RUNNING = 'Running'
 DONE    = 'Done'
 FAILED  = 'Failed'
-
-
-# ------------------------------------------------------------------------------
-#
-def lout(txt, stream=sys.stdout):
-
-    with _out_lock:
-        stream.write(txt)
-        stream.flush()
 
 
 # ------------------------------------------------------------------------------
@@ -52,7 +40,6 @@ class RLock(object):
     should be used with utter care, and rarely (such as on close()).
 
     see http://stackoverflow.com/questions/6780613/
-         is-it-possible-to-subclass-lock-objects-in-python-if-not-other-ways-to-debug
     """
 
     # --------------------------------------------------------------------------
@@ -61,48 +48,31 @@ class RLock(object):
 
         self._lock = threading.RLock()
 
-      # with self._lock:
-      #     self._obj = obj
-      #     self._cnt = 0
-
 
     # --------------------------------------------------------------------------
     #
     def acquire(self):
 
-      # ind = (self._cnt)*' '+'>'+(30-self._cnt)*' '
-      # lout("%s -- %-10s %50s acquire  - %s\n" % (ind, threading.current_thread().name, self, self._lock))
-
         self._lock.acquire()
-
-      # self._cnt += 1
-      # ind = (self._cnt)*' '+'|'+(30-self._cnt)*' '
-      # lout("%s    %-10s %50s acquired - %s\n" % (ind, threading.current_thread().name, self, self._lock))
 
 
     # --------------------------------------------------------------------------
     #
     def release(self):
 
-      # ind = (self._cnt)*' '+'-'+(30-self._cnt)*' '
-      # lout("%s    %-10s %50s release  - %s\n" % (ind, threading.current_thread().name, self, self._lock))
-
         try:
             self._lock.release()
+
         except RuntimeError as e:
             # lock has been released meanwhile - we allow that
-          # print 'ignore double lock release'
             pass
-
-      # self._cnt -= 1
-      # ind = (self._cnt)*' '+'<'+(30-self._cnt)*' '
-      # lout("%s -- %-10s %50s released - %s\n" % (ind, threading.current_thread().name, self, self._lock))
 
 
     # --------------------------------------------------------------------------
     #
     def __enter__(self)                        : self.acquire() 
     def __exit__ (self, type, value, traceback): self.release()
+
 
 
 # ------------------------------------------------------------------------------
@@ -119,7 +89,7 @@ class Thread(threading.Thread):
 
         if not callable(call):
             raise ValueError("Thread requires a callable to function, not %s" \
-                            % (str(call)))
+                            % (repr(call)))
 
         threading.Thread.__init__(self)
 
@@ -176,14 +146,6 @@ class Thread(threading.Thread):
 
     # --------------------------------------------------------------------------
     #
-    def cancel(self):
-        # FIXME: this is not really implementable generically, so we ignore 
-        # cancel requests for now.
-        pass
-
-
-    # --------------------------------------------------------------------------
-    #
     def get_state(self):
         return self._state 
 
@@ -193,6 +155,16 @@ class Thread(threading.Thread):
     # --------------------------------------------------------------------------
     #
     def get_result(self):
+        '''
+        Changing this method to 
+
+            self.join()
+            return self._result
+
+        would effectively turn this Thread class into a Future.  As it is, it is
+        implemented as a somewhat bleak future, from which you can't expect
+        much...
+        '''
 
         if  self._state == DONE:
             return self._result
