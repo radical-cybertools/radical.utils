6f437f56e093946a492101c905cf627090971c9b
Andre Merzky <andre@merzky.net>
2017-06-26 16:15:55 +0200

    add an get_size() method

--- src/radical/utils/misc.py
+++ src/radical/utils/misc.py
@@ -496,4 +496,30 @@ def stack():
     
 
 # ------------------------------------------------------------------------------
+#
+def get_size(obj, seen=None):
+
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+
+    # mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+
+    return size
+
+# ------------------------------------------------------------------------------
 
