a9e2281714af7a66a461bcc12370afe0162f1caf
Andre Merzky <andre@merzky.net>
2017-11-08 10:15:42 +0100

    move reporter "test", optional color, animation

--- tests/unittests/test_logger.py
+++ tests/unittests/test_logger.py
@@ -4,33 +4,37 @@ __copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
 
 
-''' Unit tests for ru.get_logger(name)
-'''
-
 import radical.utils as ru
 
 
-############################# BEGIN UNIT TESTS ################################
-##
+# ------------------------------------------------------------------------------
+#
 def test_singleton():
-    """ Test if the logger behaves like a singleton
-    """
+    '''
+    Test if the logger behaves like a singleton
+    '''
+
     # make sure singleton works
     assert ru.get_logger('radical.utils') == ru.get_logger('radical.utils')
 
+
+# ------------------------------------------------------------------------------
+#
 def test_logger():
-    """ Print out some messages with different log levels
-    """
-    cl = ru.get_logger('engine')
-    cl = ru.get_logger('engine')
-    cl.setLevel('DEBUG')
-    
-    assert cl is not None
-    cl.debug('debug')
-    cl.info('info')
-    cl.warn('warning')
-    cl.error('error')
-    cl.fatal('fatal')
+    '''
+    Print out some messages with different log levels
+    '''
+
+    tmp = ru.get_logger('engine')
+    tmp = ru.get_logger('engine')
+    tmp.setLevel('DEBUG')
+
+    tmp.debug('debug')
+    tmp.info('info')
+    tmp.warn('warning')
+    tmp.error('error')
+    tmp.fatal('fatal')
 
 
+# ------------------------------------------------------------------------------
 
