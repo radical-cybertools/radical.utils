215bf6debebdcb7503146fe35efdab3869e8ac30
Andre Merzky <andre@merzky.net>
2018-01-16 20:16:47 +0100

    test iteration

--- tests/unittests/test_proxy.py
+++ tests/unittests/test_proxy.py
@@ -12,7 +12,7 @@ import radical.utils as ru
 #
 def test_socks():
 
-    proxy_url = 'ssh://144.76.72.175/'
+    proxy_url = 'ssh://two.radical-project.org/'
     proxy     = ru.Proxy(url=proxy_url)
     test_url  = 'https://raw.githubusercontent.com/radical-cybertools/' \
                 'radical.utils/devel/src/radical/utils/__init__.py'
@@ -20,7 +20,7 @@ def test_socks():
 
     cmd = 'curl --socks5 %s:%s %s' % (ret_url.host, ret_url.port, test_url)
     out = sp.check_output(cmd, shell=True, stderr=sp.STDOUT)
-  # print out
+    print out
 
     assert(ret_url.host == '127.0.0.1')
     assert('version_detail' in out)
@@ -32,14 +32,29 @@ def test_socks():
 #
 def test_tunnel():
 
-    proxy_url = 'ssh://144.76.72.175/'
+    proxy_url = 'ssh://two.radical-project.org/'
     proxy     = ru.Proxy(url=proxy_url)
     test_url  = 'mongodb://am:am@ds015720.mlab.com:15720/am_rp'
     ret_url   = proxy.url(test_url, socks=False)
 
     _, db, _, _, _ = ru.mongodb_connect(ret_url)
     cols = list(db.collection_names())
-  # print cols
+    print cols
+
+    assert(ret_url.host == '127.0.0.1')
+    assert(len(cols))
+
+    proxy.close()
+
+
+# ------------------------------------------------------------------------------
+#
+def test_command_channel():
+
+    proxy_url = 'ssh://two.radical-project.org/'
+    proxy     = ru.Proxy(url=proxy_url, cmd='date >> /tmp/t')
+    test_url  = 'ssh://two.radical-project.org/'
+    ret_url   = proxy.url(test_url, socks=False)
 
     assert(ret_url.host == '127.0.0.1')
     assert(len(cols))
