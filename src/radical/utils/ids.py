2ea5f213a68eea6d6d8265fcf66fe9b6794a9e65
vivek-bala <b.vivek91@gmail.com>
2018-01-17 20:57:08 +0530

    adding note about namespace arg and changing cnt filename to ru

--- src/radical/utils/ids.py
+++ src/radical/utils/ids.py
@@ -138,6 +138,25 @@ def generate_id(prefix, mode=ID_SIMPLE, namespace=None):
     the last case though (`ID_PRIVATE`), the counter is reset for every new day,
     and can thus span multiple applications.
 
+    'namespace' argument can be specified to a value such that unique IDs are 
+    created local to that namespace, . For example, you can create a session
+    and use the session ID as a namespace for all the IDs of the objects of that
+    execution. 
+
+    Example:: 
+
+        sid   = ru.generate_id('re.session', ru.ID_PRIVATE)
+        print sid, ru.generate_id('pipeline.%(item_counter)04d', ru.ID_CUSTOM, namespace=sid)
+        print sid, ru.generate_id('pipeline.%(item_counter)04d', ru.ID_CUSTOM, namespace=sid)
+
+
+    This will generate the following output::
+
+        re.session.vivek-HP-Pavilion-m6-Notebook-PC.vivek.017548.0001 pipeline.0000
+        re.session.vivek-HP-Pavilion-m6-Notebook-PC.vivek.017548.0001 pipeline.0001
+
+    The namespaces are stored under ```$HOME/.radical/utils/```
+
     Note that for docker containers, we try to avoid hostname / username clashes
     and will, for `ID_PRIVATE`, revert to `ID_UUID`.
     """
@@ -207,7 +226,7 @@ def _generate_id(template, prefix, namespace=None):
     if '%(uuid)' in template: info['uuid'] = uuid.uuid1()         # pain old uuid
 
     if '%(day_counter)' in template:
-        fd = os.open("%s/rp_%s_%s.cnt" % (state_dir, user, days), os.O_RDWR | os.O_CREAT)
+        fd = os.open("%s/ru_%s_%s.cnt" % (state_dir, user, days), os.O_RDWR | os.O_CREAT)
         fcntl.flock(fd, fcntl.LOCK_EX)
         os.lseek(fd, 0, os.SEEK_SET )
         data = os.read(fd, 256)
@@ -218,7 +237,7 @@ def _generate_id(template, prefix, namespace=None):
         os.close(fd)
 
     if '%(item_counter)' in template:
-        fd = os.open("%s/rp_%s_%s.cnt" % (state_dir, user, prefix), os.O_RDWR | os.O_CREAT)
+        fd = os.open("%s/ru_%s_%s.cnt" % (state_dir, user, prefix), os.O_RDWR | os.O_CREAT)
         fcntl.flock(fd, fcntl.LOCK_EX)
         os.lseek(fd, 0, os.SEEK_SET)
         data = os.read(fd, 256)
