2ceeb255499e3154da8a177cf29764a72a1e8332
Andre Merzky <andre@merzky.net>
2019-01-03 13:15:13 +0100

    add missing files

---
+++ bin/ru.sh.bootstrap.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+
+# The purpose of this script is to expose a shell over a network port.  This is
+# intented to be used by the RU.SH bootstrapping routine: it will create such an
+# endpoint, and then forward the tcp port over an ssh tunnel.
+#
+#
+# Security Considerations
+# -----------------------
+#
+# Opening a shell over a network port is *obviously* a bad idea.  We apply two
+# measures to mitigate the security risks:
+#
+#   - the service endpoint will only listen on the local network interface, and
+#     will thus not be directly reachable from the public internet.
+#   - we don't interface directly to a shell, but rather to a wrapper.  That
+#     wrapper will expect a secret as first string on all commands, and will
+#     immediately terminate itself if an incorrect secret is provided.  That
+#     secret will never live on the disk (but can be spotted via `/proc`.
+#
+# Its likely that better security measures will be needed for production use.
+# 
+#
+# Implementation
+# --------------
+#
+# This script is being staged to the target resource, and is then executed when
+# an ssh tunnel is created.  It performs the following actions:
+#
+#   - find required tools and configure for common alternatives
+#     - netcat / ncat / nc
+#     - netstat / ss
+#     - /bin/sh as sh, bash, dash
+#   - check if a nc_sh_service is already active on this host (for this user,
+#     from the originating host).  If so, check if the nc shell is still alive.
+#     If so, don't start a new shell, but reconnect to it, and return the EP.
+#     FIXME: can that lead to duplicated connections?
+#   - check if secure shell wrapper script exists in $RUSH_HOME
+#     (sec_shell_wrapper.sh).  Create it if not.
+#   - create a unique named fifo in $RUSH_HOME (fifo_$$)
+#   - find an open local port and connect that port to the fifo, via netcat
+#     cat $FIFO | /bin/sh $WRAPPER -i 2>&1 | nc -l 127.0.0.1 $P > $FIFO
+#   - check if that pipeline remains alive healthy (for one sec)
+#   - echo the chosen port on stdout
+#
+# The client side of the tunnel will use that port information to open another
+# tunnel to that port, thus essentially obtaining a secured, persistent, private
+# and managed shell endpoint.  This can be used to further bootstrap the Python
+# and ZMQ layer for async command execution.
+#
+# FIXME: we should also check if that ZMQ layer already exists andis running,
+#        because we can skip most of the above in that case and then return
+#        the ZMQ endpoint.
+#
+# FIXME: Do we need to lock against concurrent incarnations of this script?
+#
+
+SECRET="$1"
+ORIGIN="$2"
+
+
+# ------------------------------------------------------------------------------
+#
+# make sure we have all env settings we need
+#
+test -z "$UID"       && UID=$(id -u)
+test -z "$TMP"       && TMP="/tmp/"
+test -z "$RUSH_TMP"  && RUSH_TMP="$TMP/.radical/ru.sh/"
+test -z "$RUSH_HOME" && RUSH_HOME="$HOME/ru.sh.$UID/"
+
+mkdir -p "$RUSH_TMP"
+mkdir -p "$RUSH_HOME"
+
+RUSH_BS1="$RUSH_HOME/rush.bs.1.sh"
+
+
+# ------------------------------------------------------------------------------
+#
+# create a shell script which executes commands on the netcat'ed port.  As
+# motivated above, we don't invoke a shell directly, to (a) add some security,
+# and (b) avoid the need for prompt parsing.
+# This script is created no matter what, wven if it already exists.  This allows
+# for seemless upgrade of this bootstrapper.  We use a `mv` command to
+# atomically replace any existing script.
+cat >  "$RUSH_BS1.$$" \
+    << EOT
+#!/bin/sh
+MD5_SECRET="$1"
+PROMPT="rush.bs.1> "
+if test -z "\$MD5_SECRET"
+then
+    echo "missing secret (md5)"
+    exit 1
+fi
+echo "\$PROMPT"
+while read -r SECRET CMD
+do
+    MD5="\$(echo -n "\$SECRET" | md5sum | cut -f 1 -d ' ')"
+    if ! test "\$MD5" = "\$MD5_SECRET"
+    then
+        echo "secret mismatch - abort"
+        exit 1
+    fi
+    # FIXME: add logging, tracing
+    RET=\$(\$CMD)
+    echo "\$RET"
+    echo "\$PROMPT"
+done
+EOT
+chmod 0700 "$RUSH_BS1.$$"
+mv "$RUSH_BS1.$$" "$RUSH_BS1"
+
+
+# ------------------------------------------------------------------------------
+#
+# find a free port and start a netcat'ed shell on it
+#
+PMIN=10000
+PMAX=11000
+P=$((PMIN-1))
+
+# get list of ports which are used - we don't need to try those (ignore races)
+used=$(netstat -lnt | awk '{print $4}' | awk -F : '{printf " %s ", $2}')
+
+# create a fifo to put the shell behind
+mkdir -p "$RUSH_HOME"
+FIFO="$RUSH_HOME/ru_${UID}_$$.fifo"
+rm -f $FIFO; mkfifo $FIFO
+
+# loop over the portrange (excl. used ports) until we find a usable one
+while test "$P" -lt "$PMAX"
+do
+    # increase to next port number, but skip if we know (knew) it's being used
+    P=$((P+1))
+    echo " $used " | grep " $P " 2>&1 > /dev/null
+    if test "$?" = 0
+    then
+        echo "skip  $P [used]"
+        continue
+    fi
+
+    # port could be free - try to start our netcat'ed shell
+    echo "check $P"
+    cat "$FIFO"                      \
+        | "$RUSH_BS1" "$SECRET" 2>&1 \
+        | nc -l 127.0.0.1 $P         \
+        >  "$FIFO"                   \
+        2> "$FIFO.err" &
+    pid=$!
+
+    # let it bootstrap, and check if it's still alive (send signal 0)
+    # NOTE: we could sleep shorter - but POSIX says 'sleep arg is INT'
+    sleep 1
+    kill -0 "$pid"
+    if test "$?" = 0
+    then
+        # the shell seems to be alive - we are done here
+        break
+    fi
+done
+
+if ! test "$P" -lt "$PMAX"
+then
+    # we either did not manage to find an open port in the given range, or the
+    # shell did not come up for some other reason.  Either way, we bail out at
+    # this point.
+    echo "NC_SH_PORT:-"
+else
+    # wohoo!
+    echo "NC_SH_PORT:$P"
+fi
+
+
+# ------------------------------------------------------------------------------
+
---
+++ src/radical/utils/sh.py
@@ -0,0 +1,374 @@
+
+__author__    = "Radical.Utils Development Team"
+__copyright__ = "Copyright 2016, RADICAL@Rutgers"
+__license__   = "MIT"
+
+
+import os
+import sys
+import glob
+import stat
+import time
+import shlex
+import textwrap
+import threading       as mt
+import subprocess      as sp
+import multiprocessing as mp
+
+from .ids import generate_id, ID_CUSTOM, ID_PRIVATE
+
+
+# ------------------------------------------------------------------------------
+#
+_TMP_ROOT = '/tmp/ru_sh_%s' % os.getuid()
+_DEF_ROOT = '%s/.radical/utils/sh' % os.environ.get('HOME')
+_ENV_ROOT = os.environ.get('RU_SH_ROOT')
+
+if   _ENV_ROOT           : _ROOT = _ENV_ROOT
+elif 'HOME' in os.environ: _ROOT = _DEF_ROOT
+else                     : _ROOT = _TMP_ROOT
+
+
+# ------------------------------------------------------------------------------
+#
+def sh_callout(cmd, shell=False, quiet=False):
+    '''
+    call a shell command, return `[stdout, stderr, retval]`.
+
+      * `cmd`  : command string to execute
+      * `shell`: run cmd in a shell (as `sh -c $cmd`)    (default: False)
+      * `quiet`: capture stdout/stderr                   (default: True )
+    '''
+
+    # convert string into arg list if needed
+    if not shell and isinstance(cmd, basestring):
+        cmd = shlex.split(cmd)
+
+    if quiet:
+        p = sp.Popen(cmd, stdout=None, stderr=None, shell=shell)
+    else:
+        p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE, shell=shell)
+
+    stdout, stderr = p.communicate()
+    return stdout, stderr, p.returncode
+
+
+# --------------------------------------------------------------------------
+#
+class Cmd(object):
+    '''
+    This class represents a command executed by a ru.SH instance.  That
+    command's state is maintained in the file system - this class only has
+    a local representation of that state.  Once the command is in any of the
+    `FINAL` states, its stdout, stderr and return value will be available.
+
+    Instances of this class should only be created by an ru.SH instances.
+    '''
+
+    # FIXME: make partial stdout/stderr available before reaching final state
+
+    # state defines
+    UNKNOWN  = 'unknown'    # before reconnect and state query
+    NEW      = 'new'        # command is defined
+    RUNNING  = 'running'    # command is being executed
+    DONE     = 'done'       # execution completed w/o error
+    FAILED   = 'failed'     # execution completed w/  error
+    CANCELED = 'canceled'   # caller canceled exection
+    PURGED   = 'purged'     # remote state will be unavailable
+
+    FINAL    = [DONE, FAILED, CANCELED]
+
+
+    # ----------------------------------------------------------------------
+    #
+    def __init__(self, sh, cmd=None, uid=None):
+
+        if not cmd and not uid:
+            raise ValueError('nothing to do, nothing to reconnect')
+
+        if cmd and uid:
+            raise ValueError('cannot *re*connect to a new command')
+
+        self._sh    = sh
+        self._cmd   = cmd
+        self._state = self.NEW
+
+        if uid:
+            self._uid = uid
+            self._pwd = '%s/%s' % (self._sh.root, self._uid)
+            self._state = self.UNKNOWN
+            self._initialize()
+            self._reconnect()
+
+        else:
+            self._uid = generate_id('cmd.%(item_counter)06d', mode=ID_CUSTOM, 
+                                    base=self._sh.root)
+            self._pwd = '%s/%s' % (self._sh.root, self._uid)
+            self._initialize()
+            self._run()
+
+
+    # ----------------------------------------------------------------------
+    #
+    def _initialize(self):
+
+        self._sh_path  = '%s/cmd.sh' % self._pwd
+        self._cmd_path = '%s/cmd'    % self._pwd
+        self._out_path = '%s/out'    % self._pwd
+        self._err_path = '%s/err'    % self._pwd
+        self._ret_path = '%s/ret'    % self._pwd
+        self._pid_path = '%s/pid'    % self._pwd
+
+        self._out  = None  # only set once the command is done
+        self._err  = None  # --             ''              --
+        self._ret  = None  # --             ''              --
+        self._pid  = None  # will be set on reconnect, too
+        self._proc = None  # on reconnect, self._proc handle remains `None`
+
+
+    # ----------------------------------------------------------------------
+    #
+    def _update(self):
+
+
+        if self._ret is None:
+            if os.path.isfile(self._ret_path):
+                try:
+                    with open(self._ret_path, 'r') as f:
+                        self._ret = int(f.read().strip())
+                    if self._ret == 0: self._state = self.DONE
+                    else             : self._state = self.FAILED
+
+                except Exception as e:
+                    self._state = -255
+                    self._state = self.FAILED
+
+            elif self._pid:
+                self._state = self.RUNNING
+
+
+        if self._state in self.FINAL and self._ret is not None:
+
+            if os.path.isfile(self._out_path):
+                with open(self._out_path, 'r') as f:
+                    self._out = f.read().rstrip()
+
+            if os.path.isfile(self._err_path):
+                with open(self._err_path, 'r') as f:
+                    self._err = f.read().rstrip()
+
+
+    # ----------------------------------------------------------------------
+    #
+    @property
+    def sh(self) : return self._sh
+    @property
+    def uid(self): return self._uid
+    @property
+    def cmd(self): return self._cmd
+
+
+    # ----------------------------------------------------------------------
+    #
+    @property
+    def state(self):
+
+        self._update()
+        return self._state
+
+
+    # ----------------------------------------------------------------------
+    #
+    @property
+    def pid(self):
+
+        if self._pid is None:
+            if os.path.isfile(self._pid_path):
+                with open(self._pid_path, 'r') as f:
+                    self._pid = int(f.read().strip())
+
+        return self._pid
+
+
+    # ----------------------------------------------------------------------
+    #
+    @property
+    def ret(self):
+
+        self._update()
+        return self._ret
+
+
+    # ----------------------------------------------------------------------
+    #
+    @property
+    def out(self):
+
+        self._update()
+        return self._out
+
+
+    # ----------------------------------------------------------------------
+    #
+    @property
+    def err(self):
+
+        self._update()
+        return self._err
+
+
+    # ----------------------------------------------------------------------
+    #
+    def _reconnect(self):
+
+        # ensure that uid and pwd are valid
+        if not os.path.isdir(self._pwd):
+            raise ValueError('invalid command uid %s' % self._uid)
+
+        with open(self._cmd_path, 'r') as fin:
+            self._cmd = str(fin.read().strip())
+
+        with open(self._pid_path, 'r') as fin:
+            self._pid = int(fin.read().strip())
+
+        self._update()
+
+
+    # ----------------------------------------------------------------------
+    #
+    def _run(self):
+        ''' 
+        Create a shell script which will execute the requested command
+        asynchronously and reconnectably in its pwd sandbox.
+        '''
+
+        if os.path.isdir(self._pwd):
+            raise RuntimeError('cannot run cmd (pwd exists: %s)' % self._pwd)
+
+        os.makedirs(self._pwd)
+
+        script = '''\
+                 #!/bin/sh
+
+                 cd %s
+                 . ./cmd 1> ./out \\
+                         2> ./err
+                 echo $?  > ./ret
+
+                 ''' % self._pwd
+
+        with open(self._sh_path, 'w') as fout:
+            fout.write(textwrap.dedent(script))
+        with open(self._cmd_path, 'w') as fout:
+            fout.write('%s\n' % self._cmd)
+
+        # make script executable
+        st = os.stat(self._sh_path)
+        os.chmod(self._sh_path, st.st_mode | stat.S_IEXEC)
+
+
+        # the script now exists and can be executed.  The subroutine below is
+        # the core of that, and will be executed in a separate, daemonized
+        # process.
+        def run_cmd():
+            self._sh._q.put(self.uid)
+            with open(self._pid_path, 'w') as fout:
+                fout.write('%d\n' % os.getpid())
+            sys.exit(sh_callout(self._sh_path, shell=True, quiet=True))
+
+        self._proc = mp.Process(target=run_cmd)
+        self._proc.daemon = True
+        self._proc.start()
+
+        # wait until we see the process, as otherwise the shell may close to
+        # quickly for the command to spawn -- calling `is_alive()` is *not*
+        # sufficient for that!
+        # Well, even that is not enough actually, we need to wait until the
+        # process is daemonized.  There is no way to check for this via the mp
+        # API, so we just sleep...  ARGHH!
+        assert(self.uid == self._sh._q.get())
+        self._state = self.RUNNING
+        time.sleep(0.1)
+
+
+# ------------------------------------------------------------------------------
+#
+class SH(object):
+    '''
+    The class represents a service endpoint which will execute shell commands
+    asynchronously.  It will store state of all tasks on disk (in `%s/$uid`).
+    As the state is thus persistent, new instances of this class can reconnect
+    to that state, by providing a `UID`.
+    ''' % _ROOT
+
+    def __init__(self, uid=None, root=None):
+        '''
+        If a session ID `uid` is given, we expect to reconnect to commands
+        started at a previous point in time, and expect the respective command
+        states to be available in the session root (`root`).
+        '''
+
+        self._uid  = uid
+        self._root = root
+
+        if  self._uid is None:
+            self._uid = generate_id('ru.sh', ID_PRIVATE)
+
+        if self._root is None:
+            self._root = '%s/%s' % (_ROOT, self._uid)
+
+        # we use a multiprocessing queue to ensure correct process spawning.
+        # Note that this requires `run()` to be guarded to maintain
+        # thread-safety.  We could also make the queue private to the `Cmd`
+        # class, but that creates an overheadd we want to avoid: keeping the
+        # queue on the `Shell` is thus an resource usage optimizaiton.
+        self._q    = mp.Queue()
+        self._lock = mt.Lock()
+
+        # make sure the state root dir exists
+        if not os.path.isdir(self._root):
+            os.makedirs(self._root, 0o700)
+
+        # pick up state for all known commands from disk
+        self._cmds = dict()
+        for cmd_dir in glob.glob('%s/cmd.*' % self.root):
+            cmd = Cmd(self, uid=os.path.basename(cmd_dir))
+            self._cmds[cmd.uid] = cmd
+
+
+    # --------------------------------------------------------------------------
+    #
+    @property
+    def root(self):
+        return self._root
+    @property
+    def uid(self):
+        return self._uid
+
+
+    # --------------------------------------------------------------------------
+    #
+    def run(self, cmd):
+
+        with self._lock:
+            ret = Cmd(self, cmd)
+            self._cmds[ret.uid] = ret
+            return ret
+
+
+    # --------------------------------------------------------------------------
+    #
+    def list(self):
+
+        return self._cmds.keys()
+
+
+    # --------------------------------------------------------------------------
+    #
+    def get(self, uid):
+
+        return self._cmds[uid]
+
+
+# ------------------------------------------------------------------------------
+
