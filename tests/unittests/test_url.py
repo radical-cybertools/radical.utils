0490fdefddb412f446c8e9f5dfdcc534a7c48b09
Andre Merzky <andre@merzky.net>
2019-03-25 10:25:47 +0100

    fix SAGA issue 305

--- tests/unittests/test_url.py
+++ tests/unittests/test_url.py
@@ -9,10 +9,7 @@ import radical.utils as ru
 
 # -------------------------------------------------------------------------
 #
-def test_url_compatibility () :
-    """
-    Test basic url compatibility
-    """
+def test_url_api():
 
     u1 = ru.Url("ssh://user:pwd@hostname.domain:9999/path")
 
@@ -25,10 +22,7 @@ def test_url_compatibility () :
 
 # ------------------------------------------------------------------------------
 #
-def test_url_scheme_issue () :
-    """
-    Test url schema issues
-    """
+def test_url_scheme_issue():
 
     u1 = ru.Url("unknownscheme://user:pwd@hostname.domain:9999/path")
 
@@ -38,23 +32,22 @@ def test_url_scheme_issue () :
     assert u1.host     == "hostname.domain"
     assert u1.port     == int(9999)
 
+
 # ------------------------------------------------------------------------------
 #
-def test_url_issue_49 () : 
-    """
-    Test url issue #49
-    """
+def test_url_issue_49():
 
     url = ru.Url ("scheme://pass:user@host:123/dir/file?query#fragment")
     url.set_host   ('remote.host.net')
-    url.set_scheme ('sftp') 
+    url.set_scheme ('sftp')
     url.set_path   ('/tmp/data')
-    
+
     assert str(url) == "sftp://pass:user@remote.host.net:123/tmp/data"
+
 
 # ------------------------------------------------------------------------------
 #
-def test_url_issue_61 () : 
+def test_url_issue_61():
     """
     Test url issue #61
     """
@@ -62,72 +55,82 @@ def test_url_issue_61 () :
     url = ru.Url ("advert://localhost/?dbtype=sqlite3")
     assert url.query == "dbtype=sqlite3"
 
+
 # ------------------------------------------------------------------------------
 #
-def test_url_properties () :
-    """
-    Test url properties
-    """
+def test_url_issue_rs_305():
+
+    url1 = ru.Url ("advert://localhost/path/to/file")
+    url2 = ru.Url ("advert://localhost//path/to/file")
+    assert url1.path == url2.path
+
+
+# ------------------------------------------------------------------------------
+#
+def test_url_properties():
 
     url = ru.Url("")
     assert str(url)           == ""
- 
+
     url.scheme = "scheme"
     assert str(url)           == "scheme://"
     assert url.get_scheme()   == "scheme"
- 
+
     url.set_scheme("tscheme")
     assert url.get_scheme()   == "tscheme"
- 
+
     url.scheme = "scheme"
     url.host   = "host"
     assert str(url)           == "scheme://host"
     assert url.get_host()     == "host"
-    
+
     url.set_host("thost")
     assert url.get_host()     == "thost"
-    
+
     url.host = "host"
     url.port = 42
     assert str(url)           == "scheme://host:42"
     assert url.get_port()     == 42
- 
+
     url.set_port(43)
     assert url.get_port()     == 43
- 
+
     url.port     = 42
     url.username = "username"
     assert str(url)           == "scheme://username@host:42"
     assert url.get_username() == "username"
-    
+
     url.set_username("tusername")
     assert url.get_username() == "tusername"
-    
+
     url.username = "username"
     url.password = "password"
     assert str(url)           == "scheme://username:password@host:42"
     assert url.get_password() == "password"
-    
+
     url.set_password("tpassword")
     assert url.get_password() == "tpassword"
-    
+
     url.password = "password"
     url.path     = "/path/"
     assert str(url)           == "scheme://username:password@host:42/path/"
     assert url.get_path()     == "/path/"
- 
+
     url.set_path("tpath")
     assert url.get_path()     == "/tpath"
-   
 
 
 # ------------------------------------------------------------------------------
 # run tests if called directly
 if __name__ == "__main__":
 
-    test_url_compatibility ()
-    test_url_scheme_issue  ()
-    test_url_issue_49      ()
-    test_url_issue_61      ()
-    test_url_properties    ()
+    test_url_api()
+    test_url_scheme_issue()
+    test_url_issue_49()
+    test_url_issue_61()
+    test_url_issue_rs_305()
+    test_url_properties()
+
+
+# ------------------------------------------------------------------------------
 
