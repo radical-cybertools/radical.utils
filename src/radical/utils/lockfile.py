8552b51601be3e1ede53d760e2d2bc3f20829aa3
Andre Merzky <andre@merzky.net>
2019-06-21 16:22:20 +0200

    add docs

--- src/radical/utils/lockfile.py
+++ src/radical/utils/lockfile.py
@@ -8,6 +8,38 @@ import fcntl
 # ------------------------------------------------------------------------------
 #
 class Lockfile(object):
+    '''
+    This class represents a lockfile.  On calling `open()` (or entering
+    a resource context `with`), the lockfile is exclusively opened and locked.
+    The returned object can be used for `read()`, `write()` and `seek()`
+    operations, and the lock is only released on `close()` (or when leaving the
+    resource context).
+
+    Example:
+
+        with ru.Lockfile(fname) as fd0:
+            assert(fd0)
+            fd0.write('test 0\n')
+
+        with ru.Lockfile(fname) as fd1:
+            assert(fd1)
+            fd1.lseek(0, os.SEEK_SET)
+            fd1.write('test 1\n')
+
+        with ru.Lockfile(fname) as fd2:
+            assert(fd2)
+            fd2.lseek(0, os.SEEK_END)
+            fd2.write('test 2\n')
+
+            # this would raise a RuntimeError
+          # with ru.Lockfile(fname) as fd3:
+          #     fd3.lseek(0, os.SEEK_END)
+          #     fd3.write('test 3\n')
+
+        with open(fname, 'r') as fin:
+            data = fin.read()
+            assert(data == 'test 1\ntest 2\n')
+    '''
 
     # --------------------------------------------------------------------------
     #
@@ -23,15 +55,15 @@ class Lockfile(object):
     def __enter__(self):
 
         if self._timeout is None:
-
-            return self._open()
+            self.open()
+            return self
 
         start = time.time()
-
         while True:
 
             try:
-                return self._open()
+                self.open()
+                return self
             except:
                 pass
 
@@ -44,7 +76,7 @@ class Lockfile(object):
 
     # --------------------------------------------------------------------------
     #
-    def _open(self):
+    def open(self):
 
         try:
             fd = os.open(self._fname, os.O_RDWR | os.O_CREAT)
@@ -63,8 +95,6 @@ class Lockfile(object):
         if not self._fd:
             raise RuntimeError('failed to lock %s' % self._fname)
 
-        return self._fd
-
 
     # --------------------------------------------------------------------------
     #
@@ -72,6 +102,44 @@ class Lockfile(object):
 
         if self._fd:
             os.close(self._fd)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def read(self, length):
+
+        if not self._fd:
+            raise ValueError('lockfile is not open')
+
+        return os.read(self._fd, length)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def write(self, data):
+
+        print self._fd
+        if not self._fd:
+            raise ValueError('lockfile is not open')
+
+        return os.write(self._fd, data)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def seek(self, pos, how):
+
+        return self.lseek(pos, how)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def lseek(self, pos, how):
+
+        if not self._fd:
+            raise ValueError('lockfile is not open')
+
+        return os.lseek(self._fd, pos, how)
 
 
     # --------------------------------------------------------------------------
--- tests/unittests/test_lockfile.py
+++ tests/unittests/test_lockfile.py
@@ -15,22 +15,22 @@ def test_lockfile():
     try:
         with ru.Lockfile(fname) as fd0:
             assert(fd0)
-            os.write(fd0, 'test 0\n')
+            fd0.write('test 0\n')
 
         with ru.Lockfile(fname) as fd1:
             assert(fd1)
-            os.lseek(fd1, 0, os.SEEK_SET)
-            os.write(fd1, 'test 1\n')
+            fd1.lseek(0, os.SEEK_SET)
+            fd1.write('test 1\n')
 
         with ru.Lockfile(fname) as fd2:
             assert(fd2)
-            os.lseek(fd2, 0, os.SEEK_END)
-            os.write(fd2, 'test 2\n')
+            fd2.lseek(0, os.SEEK_END)
+            fd2.write('test 2\n')
 
             with pytest.raises(RuntimeError):
                 with ru.Lockfile(fname) as fd3:
-                    os.lseek(fd3, 0, os.SEEK_END)
-                    os.write(fd3, 'test 3\n')
+                    fd3.lseek(0, os.SEEK_END)
+                    fd3.write('test 3\n')
 
         with open(fname, 'r') as fin:
             data = fin.read()
