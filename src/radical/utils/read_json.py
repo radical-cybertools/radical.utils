911378a2b6896a7b765feb8b6b05d052616e301c
Andre Merzky <andre@merzky.net>
2019-09-03 13:37:41 +0200

    fix json str reading

--- src/radical/utils/read_json.py
+++ src/radical/utils/read_json.py
@@ -35,12 +35,11 @@ def read_json(fname):
 #
 def read_json_str(filename):
     '''
-    same as read_json, but converts unicode strings to simple strings.
-    This could trivially done with YAML, but that introduces a compile
-    dependency -- so we convert manually...
+    same as read_json, but converts unicode strings and byte arrays to ASCII
+    strings.
     '''
 
-    return _unicode_to_strings(read_json(filename))
+    return _to_strings(read_json(filename))
 
 
 # ------------------------------------------------------------------------------
@@ -51,26 +50,9 @@ def write_json(data, filename):
 
     '''
 
-    # --------------------------------------------------------------------------
-    # thanks to
-    # http://stackoverflow.com/questions/956867/#13105359
-    def _bytes(_data):
-
-        if isinstance(_data, dict):
-            return {_bytes(key): _bytes(value) for key,value in _data.items()}
-
-        elif isinstance(_data, list):
-            return [_bytes(element) for element in _data]
-
-        elif isinstance(_data, str):
-            return _data.encode('utf-8')
-
-        else:
-            return _data
-    # --------------------------------------------------------------------------
 
     with open(filename, 'w') as f:
-        json.dump(_bytes(data), f, sort_keys=True, indent=4, ensure_ascii=False)
+        json.dump(_to_bytes(data), f, sort_keys=True, indent=4, ensure_ascii=False)
         f.write('\n')
 
 
@@ -104,30 +86,42 @@ def parse_json_str(json_str):
     same as parse_json, but converts unicode strings to simple strings
     '''
 
-    return _unicode_to_strings(parse_json(json_str))
+    return _to_strings(parse_json(json_str))
 
 
 # ------------------------------------------------------------------------------
 #
-def _unicode_to_strings(unicode_data):
+def _to_strings(data):
 
-    if isinstance(unicode_data, dict):
-        ret = dict()
-        for key, value in unicode_data.items():
-            ret[_unicode_to_strings(key)] = _unicode_to_strings(value)
-        return ret
+    if isinstance(data, dict):
+        return {_to_strings(k): _to_strings(v) for k,v in data.items()}
 
-    elif isinstance(unicode_data, list):
-        ret = list()
-        for element in unicode_data:
-            ret.append(_unicode_to_strings(element))
-        return ret
+    elif isinstance(data, list):
+        return [_to_strings(e) for e in data]
 
-    elif isinstance(unicode_data, str):
-        return unicode_data.encode('utf-8')
+    elif isinstance(data, bytes):
+        return bytes.decode(data, 'utf-8')
 
     else:
-        return unicode_data
+        return data
+
+
+# ------------------------------------------------------------------------------
+# thanks to
+# http://stackoverflow.com/questions/956867/#13105359
+def _to_bytes(data):
+
+    if isinstance(data, dict):
+        return {_to_bytes(k): _to_bytes(v) for k,v in data.items()}
+
+    elif isinstance(data, list):
+        return [_to_bytes(e) for e in data]
+
+    elif isinstance(data, str):
+        return str.encode(data, 'utf-8')
+
+    else:
+        return data
 
 
 # ------------------------------------------------------------------------------
