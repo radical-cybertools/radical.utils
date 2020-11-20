9ce48d736c97f37db8ef69046df4341de2aa65aa
Mikhail Titov <mikhail.a.titov@gmail.com>
2020-11-20 17:54:17 -0500

    proposed changes

--- src/radical/utils/plugin_manager.py
+++ src/radical/utils/plugin_manager.py
@@ -9,7 +9,7 @@ import sys
 import glob
 import pprint
 
-from importlib  import util as imp
+from importlib  import util as imp, machinery as imp_loader
 
 from .singleton import Singleton
 from .logger    import Logger
@@ -164,30 +164,12 @@ class PluginManager(object):
                 else:
                     seen.append(pshort)
 
-                # modname for 'load_source' needs to be unique, otherwise global
-                # vars in the plugin file (such as, aehm, PLUGIN_DESCRIPTION)
-                # will be overwritten by the next plugin load.
-                pmodname = '%s.plugins.%s' % (self._namespace,
-                                   os.path.basename(os.path.dirname(pfile)))
-                modname  = '%s.%s' % (pmodname,
-                                   os.path.splitext(os.path.basename(pfile))[0])
+                modname = os.path.splitext(os.path.basename(pfile))[0]
                 try:
-                    # load and register the plugin
-
-                    # modname is unique and correct -- but load_source raises
-                    # a RuntimeWarning, because, apparently, the plugin's parent
-                    # module cannot be found.  In fact, the parent is a proper
-                    # python module, and it loads fine via 'import' -- but it is
-                    # is not imported before, load_source doesn't like that.
-                    # Well, the parent module actually SHOULD NOT be imported --
-                    # but we do it here anyways to silence the warning.  Thanks
-                    # python...
-                    __import__(pmodname)
-
-                    # now load the plugin proper
-                    spec   = imp.spec_from_file_location(modname, pfile)
+                    loader = imp_loader.SourceFileLoader(modname, pfile)
+                    spec   = imp.spec_from_loader(loader.name, loader)
                     plugin = imp.module_from_spec(spec)
-                    spec.loader.exec_module(plugin)
+                    loader.exec_module(plugin)
 
                     # get plugin details from description
                     ptype  = plugin.PLUGIN_DESCRIPTION.get('type',        None)
