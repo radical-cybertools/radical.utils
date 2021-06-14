3b14cbf284251a711cc1ccf8216260bb3818d3cf
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-06-14 18:29:50 -0400

    fixes for flake8 and pylint

--- src/radical/utils/zmq/__init__.py
+++ src/radical/utils/zmq/__init__.py
@@ -4,12 +4,12 @@ __copyright__ = "Copyright 2018"
 __license__   = "GPL"
 
 
-from .client   import Client, Request,   Response
-from .bridge   import Bridge
-from .queue    import Queue,  Putter,    Getter
-from .pubsub   import PubSub, Publisher, Subscriber
-from .server   import Server
-from .registry import Registry, RegistryClient
+from .client   import Client, Request,   Response                   # noqa: F401
+from .bridge   import Bridge                                        # noqa: F401
+from .queue    import Queue,  Putter,    Getter                     # noqa: F401
+from .pubsub   import PubSub, Publisher, Subscriber                 # noqa: F401
+from .server   import Server                                        # noqa: F401
+from .registry import Registry, RegistryClient                      # noqa: F401
 
 
 # ------------------------------------------------------------------------------
--- tests/unittests/test_zmq_client.py
+++ tests/unittests/test_zmq_client.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-value-for-parameter
 
 import msgpack
 
