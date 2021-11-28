ea9e9c5745ac5d826bd3377e282bc0177b5b5189
Andre Merzky <andre@merzky.net>
2021-11-28 18:41:51 +0100

    add pipe test

--- src/radical/utils/zmq/__init__.py
+++ src/radical/utils/zmq/__init__.py
@@ -6,10 +6,11 @@ __copyright__ = "Copyright 2018"
 __license__   = "GPL"
 
 
-from .client   import Client, Request,   Response
 from .bridge   import Bridge
 from .queue    import Queue,  Putter,    Getter
 from .pubsub   import PubSub, Publisher, Subscriber
+from .pipe     import Pipe
+from .client   import Client, Request,   Response
 from .server   import Server
 from .registry import Registry, RegistryClient
 
