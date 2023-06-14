60f84498dcee219caf075ca500ec818f74c17a79
Andre Merzky <andre@merzky.net>
2023-06-14 23:26:46 +0200

    add missing file

---
+++ src/radical/utils/zmq/message.py
@@ -0,0 +1,60 @@
+
+from typing import Optional, Dict, Any
+
+import msgpack
+
+from ..typeddict import TypedDict
+
+
+# ------------------------------------------------------------------------------
+#
+class Message(TypedDict):
+
+    _schema = {
+        'msg_type': str,
+        'payload' : {str, None}
+    }
+
+    _defaults = {
+        'msg_type': None,
+        'payload' : None
+    }
+
+    _msg_types = dict()
+
+
+    # --------------------------------------------------------------------------
+    def verify(self):
+        assert self.msg_type
+        super().verify()
+
+
+    @staticmethod
+    def register_msg_type(msg_type, msg_class):
+        Message._msg_types[msg_type] = msg_class
+
+
+    @staticmethod
+    def deserialize(data: Dict[str, Any]):
+
+        msg_type = data.get('msg_type')
+
+        if msg_type is None:
+            raise ValueError('no message type defined')
+
+        if msg_type not in Message._msg_types:
+            raise ValueError('unknown message type [%s]' % msg_type)
+
+        return Message._msg_types[msg_type](from_dict=data)
+
+
+    def packb(self):
+        return msgpack.packb(self)
+
+    @staticmethod
+    def unpackb(bdata):
+        return Message.deserialize(msgpack.unpackb(bdata))
+
+
+# ------------------------------------------------------------------------------
+
