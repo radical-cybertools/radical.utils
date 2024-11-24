0f0485281ebc47f9835d5720c18f806f0f7b9cb4
Andre Merzky <andre@merzky.net>
2024-11-24 22:58:48 +0100

    get better port numbers to simplify tunneling

--- src/radical/utils/zmq/queue.py
+++ src/radical/utils/zmq/queue.py
@@ -12,7 +12,7 @@ from ..atfork    import atfork
 from ..config    import Config
 from ..ids       import generate_id, ID_CUSTOM
 from ..url       import Url
-from ..misc      import as_string, as_bytes, as_list, noop
+from ..misc      import as_string, as_bytes, as_list, noop, find_port
 from ..host      import get_hostip
 from ..logger    import Logger
 from ..profile   import Profiler
@@ -20,7 +20,7 @@ from ..debug     import print_exception_trace
 from ..serialize import to_msgpack, from_msgpack
 
 from .bridge     import Bridge
-from .utils      import no_intr
+from .utils      import zmq_bind, no_intr
 from .utils      import log_bulk, LOG_ENABLED
 # from .utils    import prof_bulk
 
@@ -165,30 +165,18 @@ class Queue(Bridge):
 
         self._log.info('start bridge %s', self._uid)
 
-        self._lock        = mt.Lock()
+        self._lock       = mt.Lock()
 
-        self._ctx         = zmq.Context()  # rely on GC for destruction
-        self._put         = self._ctx.socket(zmq.PULL)
-        self._put.linger  = _LINGER_TIMEOUT
-        self._put.hwm     = _HIGH_WATER_MARK
-        self._get.bind('tcp://*:%s' % (self._cfg.get('port_put') or find_port())
+        self._ctx        = zmq.Context()  # rely on GC for destruction
+        self._put        = self._ctx.socket(zmq.PULL)
+        self._put.linger = _LINGER_TIMEOUT
+        self._put.hwm    = _HIGH_WATER_MARK
+        self._addr_put   = zmq_bind(self._put)
 
         self._get        = self._ctx.socket(zmq.REP)
         self._get.linger = _LINGER_TIMEOUT
         self._get.hwm    = _HIGH_WATER_MARK
-        self._get.bind('tcp://*:%s' % (self._cfg.get('port_get') or find_port())
-
-        # communicate the bridge ports to the parent process
-        _addr_put = as_string(self._put.getsockopt(zmq.LAST_ENDPOINT))
-        _addr_get = as_string(self._get.getsockopt(zmq.LAST_ENDPOINT))
-
-        # store addresses
-        self._addr_put = Url(_addr_put)
-        self._addr_get = Url(_addr_get)
-
-        # use the local hostip for bridge addresses
-        self._addr_put.host = get_hostip()
-        self._addr_get.host = get_hostip()
+        self._addr_get   = zmq_bind(self._get)
 
         self._log.info('bridge in  %s: %s', self._uid, self._addr_put)
         self._log.info('bridge out %s: %s', self._uid, self._addr_get)
@@ -450,7 +438,7 @@ class Getter(object):
             qname = 'default'
 
         assert url in Getter._callbacks
-        time.sleep(1)
+        time.sleep(0.1)
 
         try:
             term = Getter._callbacks.get(url, {}).get('term')
