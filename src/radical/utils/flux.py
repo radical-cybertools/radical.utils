4101b05248006fde90131704ed2bb3117e1149c9
Andre Merzky <andre@merzky.net>
2025-03-29 13:18:01 +0100

    snap

--- src/radical/utils/flux.py
+++ src/radical/utils/flux.py
@@ -1,4 +1,5 @@
 
+import math
 import time
 import shlex
 import queue
@@ -28,21 +29,76 @@ except Exception as e:
     _flux_exc = e
 
 
+# --------------------------------------------------------------------------
+#
+def spec_from_command(cmd: str) -> 'flux.job.JobspecV1':
+
+    spec = _flux_job.JobspecV1.from_command(shlex.split(cmd))
+    spec.attributes['user']['uid'] = generate_id(ID_SIMPLE)
+
+    return spec
+
+
+# --------------------------------------------------------------------------
+#
+def spec_from_dict(td: dict) -> 'flux.job.JobspecV1':
+
+    version = 1
+    user    = {'uid'     : td.get('uid', generate_id(ID_SIMPLE))}
+    system  = {'duration': td.get('duration', 0.0)}
+    tasks   = [{'command': [td['executable']] + td.get('arguments', []),
+                'slot'   : 'task',
+                'count'  : {'per_slot': 1}}]
+
+    if 'environment' in td: system['environment'] = td['environment']
+    if 'sandbox'     in td: system['cwd']         = td['sandbox']
+    if 'shell'       in td: system['shell']       = td['shell']
+    if 'stdin'       in td: system['stdin']       = td['stdin']
+    if 'stdout'      in td: system['stdout']      = td['stdout']
+    if 'stderr'      in td: system['stderr']      = td['stderr']
+
+    attributes = {'system' : system,
+                  'user'   : user}
+    resources  = [{'count': td.get('ranks', 1),
+                   'type' : 'slot',
+                   'label': 'task',
+                   'with' : [{
+                       'count': int(td.get('cores_per_rank', 1)),
+                       'type' : 'core'}]}]
+                 #     'count': int(td.get('gpus_per_rank', 0)) or None,
+                 #     'type' : 'gpu'
+
+    gpr = td.get('gpus_per_rank', 0)
+    if gpr:
+        resources[0]['with'].append({'count': math.ceil(gpr),  # flux needs int
+                                     'type' : 'gpu'})
+
+    spec = _flux_job.JobspecV1(resources=resources,
+                               attributes=attributes,
+                               tasks=tasks,
+                               version=version)
+    return spec
+
+
 # ------------------------------------------------------------------------------
 #
-class _FluxService(object):
+class FluxService(object):
 
     # --------------------------------------------------------------------------
     #
-    def __init__(self, uid : str,
-                       log : Logger) -> None:
+    def __init__(self, uid     : str    = None,
+                       log     : Logger = None,
+                       launcher: str    = None
+                       ) -> None:
 
-        self._uid  = uid
-        self._log  = log
+        self._uid      = uid      or generate_id('ru.flux')
+        self._log      = log      or Logger('radical.utils.flux')
+        self._launcher = launcher or ''
 
-        self._fexe = which('flux')
-        self._uri  = None
-        self._tout = 60
+        self._fexe  = which('flux')
+        self._uri   = None
+        self._proc  = None
+        self._ready = mt.Event()
 
         if not _flux:
             raise RuntimeError('flux module not found') from self._exception
@@ -53,22 +109,16 @@ class _FluxService(object):
         if not self._fexe:
             raise RuntimeError('flux executable not found')
 
-        self._start()
-
 
     # --------------------------------------------------------------------------
     #
     @property
-    def uri(self) -> str:
-        return self._uri
+    def uid(self) -> str:
+        return self._uid
 
     @property
-    def timeout(self) -> int:
-        return self._tout
-
-    @timeout.setter
-    def timeout(self, tout) -> None:
-        self._tout = tout
+    def uri(self) -> str:
+        return self._uri
 
 
     # --------------------------------------------------------------------------
@@ -81,20 +131,30 @@ class _FluxService(object):
         for line in lines:
             if line.startswith('FLUX_URI:'):
                 self._uri = line.strip().split(':', 1)[1]
+                self._log.info('%s: found flux uri: %s', self._uid, self.uri)
+                self._ready.set()
 
 
     # --------------------------------------------------------------------------
     #
     def _proc_state_cb(self, proc: Process, state: str) -> None:
+
         self._log.info('flux instance state update: %s', state)
+        if state in Process.FINAL:
+
+            self._log.info('flux instance stopped: %s', state)
+            self.stop()
 
 
     # --------------------------------------------------------------------------
     #
-    def _start(self) -> None:
+    def start(self, timeout: float = None) -> None:
 
         fcmd = 'echo FLUX_URI:\\$FLUX_URI && sleep inf'
         cmd  = '%s start bash -c "%s"' % (self._fexe, fcmd)
+
+        if self._launcher:
+            cmd = '%s %s' % (self._launcher, cmd)
 
         self._log.info('%s: start flux instance: %s', self._uid, cmd)
 
@@ -105,15 +165,35 @@ class _FluxService(object):
         p.polldelay = 0.1
         p.start()
 
-        start = time.time()
-        while time.time() - start < self._tout:
-            time.sleep(0.1)
-            if self._uri is not None:
-                break
+        self._proc  = p
+        self._ptime = time.time()
 
-        if self.uri is None:
-            self._log.error('%s: flux instance did not start', self._uid)
-            raise RuntimeError('%s: flux instance did not start', self._uid)
+        return self.ready(timeout=timeout)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def ready(self, timeout: float = None) -> None:
+
+        if timeout is not None:
+            if timeout < 0: self._ready.wait()
+            else          : self._ready.wait(timeout)
+
+        return self._ready.is_set()
+
+
+    # --------------------------------------------------------------------------
+    #
+    def stop(self) -> None:
+
+        if not self._proc:
+            return
+
+        self._proc.cancel()
+        self._proc.wait()
+
+        self.uri   = None
+        self._proc = None
 
         self._log.info('%s: found flux uri: %s', self._uid, self.uri)
 
@@ -124,14 +204,15 @@ class FluxHelper(object):
 
     # --------------------------------------------------------------------------
     #
-    def __init__(self, uri : str      = None,
-                       log : Logger   = None) -> None:
+    def __init__(self, uri : str,
+                       log : Logger = None) -> None:
+
+        self._t0 = time.time()
 
         self._uri      = uri
         self._log      = log  or Logger('radical.utils.flux')
         self._uid      = generate_id('ru.flux')
-        self._handle   = None
-        self._service  = None
+        self._handle   = _flux.Flux(self._uri)
 
         # journal watcher
         self._jthread  = None
@@ -161,16 +242,10 @@ class FluxHelper(object):
 
     # --------------------------------------------------------------------------
     #
-    def start(self) -> None:
+    def start(self, launcher: str   = None) -> None:
 
-        if self._handle is not None:
+        if self._jthread is not None:
             return
-
-        if not self._uri:
-            self._flux_service = _FluxService(uid=self._uid, log=self._log)
-            self._uri = self._flux_service.uri
-
-        self._handle = _flux.Flux(self._uri)
 
         self._jthread = mt.Thread(target=self._jwatcher)
         self._jthread.daemon = True
@@ -205,6 +280,10 @@ class FluxHelper(object):
     def uid(self) -> str:
         return self._uid
 
+    @property
+    def uri(self) -> str:
+        return self._uri
+
 
     # --------------------------------------------------------------------------
     #
@@ -234,24 +313,25 @@ class FluxHelper(object):
 
         # if we get new specs, submit them, return IDs to iqueue, and also
         # forward ID to ewatcher
-
         fh = _flux.Flux(self._uri)
         while True:
 
             try:
-                specs = self._squeue.get(timeout=1.0)
+                specs = self._squeue.get(block=True, timeout=1.0)
 
             except queue.Empty:
                 continue
 
-            try:
-                with self._idlock:
+            except Exception:
+                self._log.exception("exception")
+                raise
+
+            with self._idlock:
+                try:
 
                     futs = list()
-                    tids = list()
-
                     for spec in specs:
-                        tid = spec.attributes['system']['job']['name']
+                        tid = spec.attributes['user']['uid']
                         fut = _flux_job.submit_async(fh, spec, waitable=True)
 
                         futs.append([fut, tid])
@@ -264,12 +344,14 @@ class FluxHelper(object):
                         # trigger an event check
                         self._equeue.put(fid)
 
-                # trigger submit completion
-                self._sevent.set()
 
-            except Exception:
-                self._log.exception("exception")
-                raise
+                except Exception:
+                    self._log.exception("exception")
+                    raise
+
+                finally:
+                    # trigger submit completion
+                    self._sevent.set()
 
 
     # --------------------------------------------------------------------------
@@ -294,6 +376,7 @@ class FluxHelper(object):
     def register_cb(self, cb: callable) -> None:
 
         with self._elock:
+            self._log.debug('==== register cb %s', cb)
             self._cbacks.append(cb)
 
 
@@ -314,6 +397,8 @@ class FluxHelper(object):
 
         with self._elock:
 
+            self._log.debug_9('==== event %s: %s', fid, event)
+
             # if triggered by submit, check if we have anything to do
             if not event:
                 if fid not in self._events:
@@ -321,106 +406,61 @@ class FluxHelper(object):
 
             # check if we can handle the event - otherwise store it
             if not self._cbacks:
+                self._log.debug('==== no cb %s: %s', fid, event)
                 self._events[fid].append(event)
                 return
 
             # check if application knows the task - otherwise store the event
             if fid not in self._task_ids:
+                self._log.debug('==== no id %s: %s', fid, event)
                 self._events[fid].append(event)
                 return
 
             # task is known, flush stored events
             for ev in self._events[fid]:
+                self._log.debug('==== play  %s: %s - %s', fid, event, self._cbacks)
                 for cb in self._cbacks:
                     cb(self._task_ids[fid], ev)
                 self._events[fid] = []
 
             # process the current event
             if event:
+                self._log.debug('==== relay %s: %s - %s', fid, event, self._cbacks)
                 for cb in self._cbacks:
                     cb(self._task_ids[fid], event)
 
 
     # --------------------------------------------------------------------------
     #
-    def spec_from_command(self, cmd: str) -> 'flux.job.JobspecV1':
+    def submit(self, specs: List['flux.job.JobspecV1']) -> List[str]:
 
-        return _flux_job.JobspecV1.from_command(shlex.split(cmd))
+        if not self._handle:
+            raise RuntimeError('flux instance not started')
 
-
-    # --------------------------------------------------------------------------
-    #
-    def spec_from_dict(self, td: dict) -> 'flux.job.JobspecV1':
-
-        version    = 1
-        tasks      = [{'command': [td['executable']] + td.get('arguments', []),
-                       'slot'   : 'task',
-                       'count'  : {'per_slot': 1}}]
-
-        system = {'duration': td.get('duration', 0.0)}
-
-        if 'environment' in td: system['environment'] = td['environment']
-        if 'sandbox'     in td: system['cwd']         = td['sandbox']
-        if 'shell'       in td: system['shell']       = td['shell']
-        if 'stdin'       in td: system['stdin']       = td['stdin']
-        if 'stdout'      in td: system['stdout']      = td['stdout']
-        if 'stderr'      in td: system['stderr']      = td['stderr']
-        if 'uid'         in td: system['job']         = {'name': td['uid']}
-
-        attributes = {'system' : system}
-        resources  = [{'count': td.get('ranks', 1),
-                       'type' : 'slot',
-                       'label': 'task',
-                       'with' : [{
-                           'count': int(td.get('cores_per_rank', 1)),
-                           'type' : 'core'}]}]
-                     #     'count': int(td.get('gpus_per_rank', 0)) or None,
-                     #     'type' : 'gpu'
-
-        if 'gpus_per_rank' in td:
-            resources[0]['with'].append({
-                    # flux likes integer GPU counts
-                    'count': math.ceil(td['gpus_per_rank']),
-                    'type' : 'gpu'})
-
-      # import json
-      # return json.dumps({
-      #     'version'   : version,
-      #     'resources' : resources,
-      #     'attributes': attributes,
-      #     'tasks'     : tasks})
-
-        spec = _flux_job.JobspecV1(resources=resources,
-                                   attributes=attributes,
-                                   tasks=tasks,
-                                   version=version)
-        return spec
-
-
-    # --------------------------------------------------------------------------
-    #
-    def submit(self, specs: List[Dict[str, Any]]) -> List[str]:
-
-        # ensure we have a job name which we use as task ID
+        self._log.debug('submit %d specs', len(specs))
+        tids = list()
         for spec in specs:
-            tid = spec.attributes['system'].get('job', {}).get('name')
+            tid = spec.attributes['user'].get('uid')
             if not tid:
                 tid = generate_id(ID_SIMPLE)
-                if 'job' not in spec.attributes['system']:
-                    spec.attributes['system']['job'] = dict()
-                spec.attributes['system']['job']['name'] = tid
+                if 'user' not in spec.attributes:
+                    spec.attributes['user'] = dict()
+                spec.attributes['user']['uid'] = tid
+            tids.append(tid)
 
         self._sevent.clear()
         self._squeue.put(specs)
         self._sevent.wait()  # FIXME: timeout?
 
-        tids = [spec.attributes['system']['job']['name'] for spec in specs]
         return tids
 
 
     # --------------------------------------------------------------------------
     #
     def cancel(self, tids: [str|List[str]]) -> None:
+
+        if not self._handle:
+            raise RuntimeError('flux instance not started')
 
         with self._idlock:
             for tid in as_list(tids):
@@ -432,10 +472,15 @@ class FluxHelper(object):
     #
     def wait(self, tids: [str|List[str]]) -> None:
 
+        if not self._handle:
+            raise RuntimeError('flux instance not started')
+
+        tids = as_list(tids)
         with self._idlock:
-            for tid in as_list(tids):
-                fid = self._flux_ids[tid]
-                _flux_job.wait(self._handle, fid)
+            fids = [self._flux_ids[tid] for tid in tids]
+
+        for fid in fids:
+            _flux_job.wait(self._handle, fid)
 
 
 # ------------------------------------------------------------------------------
